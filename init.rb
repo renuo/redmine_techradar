de8ffb18a761564edca32b236a6fd28e677625a5
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-04-23 13:29:14 +0200

    Update plugin metadata

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_techradar do
   name 'Redmine Techradar plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Brendan Minder'
+  description 'A Techradar plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/renuo/redmine_techradar'
+  author_url 'https://github.com/ddbrendan'
 end
