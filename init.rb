a132ea6e152ba731b995db08cdf65996059a4744
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-05-07 18:13:44 +0200

    Register permissions

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Redmine::Plugin.register :redmine_techradar do
   name 'Redmine Techradar plugin'
   author 'Brendan Minder'
@@ -5,4 +7,13 @@ Redmine::Plugin.register :redmine_techradar do
   version '0.0.1'
   url 'https://github.com/renuo/redmine_techradar'
   author_url 'https://github.com/ddbrendan'
+
+  permission :view_tech_radar,
+             { tech_radar: [:index] },
+             global: true,
+             require: :loggedin
+  permission :rate_technologies,
+             { tech_radar_ratings: [:show, :update, :skip, :back] },
+             global: true,
+             require: :loggedin
 end
