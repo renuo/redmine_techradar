cdbe381bb1197feb136c56cfd4667f6d630b546f
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-05-13 12:09:13 +0200

    Add evaluation page with scatter chart

---
+++ assets/javascripts/scatter_chart_controller.js
@@ -0,0 +1,47 @@
+import { Controller } from '@hotwired/stimulus'
+import Chart from 'chart.js/auto'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+
+export default class extends Controller {
+  static values = {
+    points: Array,
+    canLabel: String,
+    wantLabel: String
+  }
+
+  connect() {
+    const data = {
+      datasets: [{
+        data: this.pointsValue.map((p) => ({ x: p.want, y: p.can, label: p.name })),
+        backgroundColor: '#2a8a4a',
+        clip: false
+      }]
+    }
+
+    this.chart = new Chart(this.element, {
+      type: 'scatter',
+      data,
+      plugins: [ChartDataLabels],
+      options: {
+        layout: { padding: { top: 24, right: 48 } },
+        scales: {
+          x: { title: { display: true, text: this.wantLabelValue }, min: 1, max: 5, ticks: { stepSize: 1 } },
+          y: { title: { display: true, text: this.canLabelValue }, min: 1, max: 4, ticks: { stepSize: 1 } }
+        },
+        plugins: {
+          legend: { display: false },
+          datalabels: {
+            align: 'top',
+            offset: 4,
+            clip: false,
+            formatter: (_, ctx) => ctx.dataset.data[ctx.dataIndex].label
+          }
+        }
+      }
+    })
+  }
+
+  disconnect() {
+    this.chart?.destroy()
+  }
+}
