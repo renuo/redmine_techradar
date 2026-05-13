03d63197120714c974663921165432b8f83179c1
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-05-13 14:18:53 +0200

    Fix rubocop error

--- test/system/rating_workflow_test.rb
+++ test/system/rating_workflow_test.rb
@@ -80,7 +80,8 @@ class RatingWorkflowTest < ApplicationSystemTestCase
     assert_selector 'h2', text: 'Rails'
     click_button 'Back'
 
-    assert_selector 'h2', text: 'Ruby'
+    find('h2', text: 'Ruby')
+
     assert_selector 'button.previous', count: 2 # wait for Stimulus to mark both previous can/want buttons
     click_button '4. Professional'
     click_button '5. Yes'
