9bf36921145d31a6b0ef5af848d4b730d31ef54e
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-06-09 12:35:59 +0200

    Add test users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,29 @@ end
 ].each do |name|
   TechRadar::Technology.find_or_create_by!(name: name)
 end
+
+# A role only grants its permissions through a project membership, even when the permission is global.
+# Create a project and one test user per role.
+project = Project.find_or_create_by!(identifier: 'tech-radar') do |p|
+  p.name = 'Tech Radar'
+end
+
+TEST_USERS = {
+  'techradar_dev' => { lastname: 'Entwickler', role: 'Tech Radar Entwickler' },
+  'techradar_sales' => { lastname: 'Sales', role: 'Tech Radar Sales' }
+}.freeze
+
+TEST_USERS.each do |login, attrs|
+  user = User.find_or_create_by!(login: login) do |u|
+    u.firstname = 'Tech Radar'
+    u.lastname = attrs[:lastname]
+    u.mail = "#{login}@example.com"
+    u.password = 'techradar123'
+    u.status = User::STATUS_ACTIVE
+  end
+
+  unless Member.exists?(user: user, project: project)
+    Member.create!(user: user, project: project,
+                   roles: [Role.find_by(name: attrs[:role])])
+  end
+end
