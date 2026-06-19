7dee8aa80790b83201f514bfc9877e8f4c13d689
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-06-19 12:08:14 +0200

    Add My ratings overview page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@
 
 get 'tech_radar', to: 'tech_radar#index', as: :tech_radar
 
+get 'tech_radar/ratings', to: 'tech_radar_ratings#index', as: :tech_radar_ratings
+patch 'tech_radar/ratings/:technology_id',
+      to: 'tech_radar_ratings#save', as: :save_tech_radar_rating
+
 resource :tech_radar_rating,
          only: [:show, :update],
          path: 'tech_radar/rate',
