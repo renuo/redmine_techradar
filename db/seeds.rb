7c0100b862b8a4b3dd55178306a7d4ba403b4655
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-06-22 11:34:47 +0200

    Add all technologies from spreadsheet to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,40 @@ ROLE_PERMISSIONS.each do |name, permissions|
 end
 
 [
-  'Ruby', 'Ruby on Rails', 'PostgreSQL', 'Redis', 'Stimulus',
-  'Hotwire Turbo', 'TypeScript', 'React', 'Tailwind CSS',
-  'Docker', 'Kubernetes', 'Terraform', 'GraphQL', 'gRPC'
+  'JavaScript', 'SQL', 'HTML/CSS', 'Python', 'TypeScript', 'Bash/Shell',
+  'Java', 'C#', 'C++', 'PHP', 'C', 'Go', 'PowerShell', 'Rust', 'Kotlin',
+  'Dart', 'Ruby', 'Lua', 'Swift', 'Visual Basic', 'Assembly', 'Groovy',
+  'VBA', 'R', 'MATLAB', 'Scala', 'Objective-C', 'Perl', 'Elixir', 'Delphi',
+  'GDScript', 'Haskell', 'Clojure', 'Lisp', 'MicroPython', 'Solidity',
+  'Erlang', 'Zig', 'F#', 'Fortran', 'Apex', 'Julia', 'Ada', 'Prolog',
+  'Cobol', 'OCaml', 'Crystal', 'Zephyr', 'Nim',
+  'PostgreSQL', 'MySQL', 'SQLite', 'Microsoft SQL Server', 'MongoDB',
+  'Redis', 'MariaDB', 'Elasticsearch', 'Oracle', 'Dynamodb',
+  'Firebase Realtime Database', 'Cloud Firestore', 'BigQuery', 'H2',
+  'Supabase', 'Cosmos DB', 'Microsoft Access', 'Snowflake', 'InfluxDB',
+  'Cassandra', 'Databricks SQL', 'Neo4J', 'Clickhouse', 'IBM DB2', 'Solr',
+  'DuckDB', 'Firebird', 'Couch DB', 'Cockroachdb', 'Couchbase', 'Presto',
+  'Datomic', 'EventStoreDB', 'RavenDB', 'TiDB',
+  'React', 'Node.js', 'jQuery', 'Angular', 'ASP.NET CORE', 'Next.js',
+  'Express', 'Vue.js', 'ASP.NET', 'Spring Boot', 'Flask', 'Django',
+  'WordPress', 'FastAPI', 'Laravel', 'AngularJS', 'NestJS', 'Svelte',
+  'Blazor', 'Ruby on Rails', 'Nuxt.js', 'Symfony', 'Htmx', 'Astro',
+  'Fastify', 'Phoenix', 'Drupal', 'Strapi', 'Deno', 'CodeIgniter',
+  'Gatsby', 'Remix', 'Solid.js', 'Yii 2', 'Play Framework', 'Elm', '.NET',
+  'Pandas', 'NumPy', '.NET Framework', 'RabbitMQ', 'Spring Framework',
+  'Apache Kafka', 'Flutter', 'Torch/PyTorch', 'React Native',
+  'Scikit-Learn', 'TensorFlow', 'OpenCV', 'Qt', 'Electron', 'OpenGL',
+  'CUDA', 'Apache Spark', 'SwiftUI', 'Hugging Face Transformers', 'Keras',
+  '.NET MAUI', 'Xamarin', 'Ruff', 'Ionic', 'Cordova', 'Hadoop', 'Tauri',
+  'GTK', 'Capacitor', 'Roslyn', 'DirectX', 'Quarkus', 'OpenCL', 'Ktor',
+  'mlflow', 'MFC', 'Tidyverse', 'JAX',
+  'Docker', 'npm', 'Pip', 'Homebrew', 'Kubernetes', 'Yarn', 'Webpack',
+  'Vite', 'Make', 'NuGet', 'Maven', 'Gradle', 'Visual Studio Solution',
+  'MSBuild', 'Terraform', 'Composer', 'APT', 'pnpm', 'Chocolatey',
+  'Ansible', 'Pacman', 'Podman', 'Unity 3D', 'Ninja', 'Bun', 'Godot',
+  'Google Test', 'Ant', 'Nix', 'Unreal Engine', 'Dagger', 'Puppet',
+  'Pulumi', 'Chef', 'Solidus / Spree', 'THREE.js', 'Fauna', 'Stimulus',
+  'Github Actions', 'Turbo', 'GitLab CI', 'nostr'
 ].each do |name|
   TechRadar::Technology.find_or_create_by!(name: name)
 end
