d707f116b7cd69ba7c24998e4dfbd859e89e03bb
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2026-06-24 10:52:40 +0200

    Use turbo-rails gem and turbo_frame_tag helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,8 @@ SimpleCov.start do
 end
 
 require File.expand_path('../../../test/test_helper', __dir__)
+
+# turbo-rails mixes ActionCable::TestHelper into every test case, but Redmine
+# configures no Action Cable adapter. Point it at the in-memory test adapter so
+# the helper's before_setup can build a pubsub.
+ActionCable.server.config.cable ||= { 'adapter' => 'test' }
